513581710cff47aac3d4b6bd9b85d64d97ef7b95
JosueLft <josueflx1999@gmail.com>
2023-03-30 19:01:02 -0300

    Criação do projeto

---
+++ application-API/src/main/java/com/reign/lofty/applicationAPI/ApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.reign.lofty.applicationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationApiApplication.class, args);
+	}
+
+}
---
+++ application-API/src/test/java/com/reign/lofty/applicationAPI/ApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reign.lofty.applicationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
